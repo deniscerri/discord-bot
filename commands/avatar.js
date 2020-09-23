50568c38e454a69aaa7c4ec0ce4c5036199595d8
deniscerri <deniscerri@users.noreply.github.com>
2020-09-23 18:12:09 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,10 +5,11 @@ module.exports = {
 	description: 'Shows Avatar!',
 	execute(message, args) {
       const user = message.mentions.users.first() || message.author;
+      const image = user.displayAvatarURL({dynamic: true, size: 4096});
                 const avatarEmbed = new Discord.MessageEmbed()
-                    .setAuthor(user.username, '', user.avatarURL)
-                    .setImage(user.avatarURL({dynamic: true, size: 4096}));
-                    
+                    .setTitle('URL')
+                    .setURL(image)
+                    .setImage(image);
                 message.channel.send(avatarEmbed);
 	},
 };
