ce987035ab8b0b37bc09b332a3947bb196932214
deniscerri <deniscerri@users.noreply.github.com>
2020-09-25 22:09:05 +0200

    New way to get memes from reddit
    Ditched the heroku api, used the original reddit api. It gives me flexibility and more options to show on the output.

--- commands/meme.js
+++ commands/meme.js
@@ -1,45 +1,65 @@
+
+const Discord = require("discord.js");
 const fetch = require('node-fetch');
-const Discord = require('discord.js');
 
-
+let reddit = [
+    "dankruto",
+    "memes",
+    "funny",
+    "animememes",
+    "dankmemes",
+    "wholesomememes",
+    "meirl",
+    "2meirl4meirl"
+    
+]
 
 module.exports = {
     name: 'meme',
-    aliases: ['memes','meem','mem'],
+    aliases: ['memes','meem','mem','memw'],
     description: 'Send Memes',
     execute(message, args) {
-      meme(message);
+      
+      
+   
+    let subreddit = '';
+    if(args == ''){
+        subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+    }else{
+        subreddit = args;
+    }
+
+    
+    postMeme(message,subreddit);
+
+    
+
       return;
   },
 };
 
 
-async function meme(message){
+function postMeme(message, subreddit){
+    fetch(`https://www.reddit.com/r/${subreddit}.json?sort=top&t=week`)
+    .then(res => res.json())
+    .then(json => {
 
-     let reddit = [
-        "dankruto",
-        "memes",
-        "funny",
-        "animememes",
-        "dankmemes",
-        "wholesomememes",
-        "meirl",
-        "2meirl4meirl"
+
+        if(json.data.dist == 0){
+            message.channel.send("Couldn't find subreddit.");
+        }else{
+            var random = Math.floor(Math.random() * (json.data.children).length);
+
+            console.log((json.data.children)[random]);
+            
+            let embed = new Discord.MessageEmbed()
+                .setTitle((json.data.children)[random].data.title)
+                .setURL(`https://reddit.com${(json.data.children)[random].data.permalink}`)
+                .setImage((json.data.children)[random].data.url)
+                .setFooter(`👍 ${(json.data.children)[random].data.ups} 💬 ${(json.data.children)[random].data.num_comments} | Subreddit: r/ ${(json.data.children)[random].data.subreddit}`)
+             message.channel.send(embed)
+        }
         
-    ]
-
-    let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
-
-        fetch(`https://meme-api.herokuapp.com/gimme/${subreddit}`)
-            .then(res => res.json())
-            .then(json => {
-                
-                let embed = new Discord.MessageEmbed()
-                    .setTitle(json.title)
-                    .setURL(json.postLink)
-                    .setImage(json.url)
-                    .setFooter(`👍 ${json.ups} | Subreddit: r/ ${json.subreddit}`)
-                message.channel.send(embed)
-            });
+       
+    });
 }
-
