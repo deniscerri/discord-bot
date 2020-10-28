d64bd4b6126f72d140d26705ba77cb9304c87aef
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2020-10-28 21:12:21 +0100

    Removed 'urban' dependency
    Fetched info from the API using node-fetch instead.

--- commands/define.js
+++ commands/define.js
@@ -1,35 +1,39 @@
-const urban = module.require("urban");
+const fetch = require('node-fetch')
 
 
 module.exports = {
 	name: 'define',
   aliases: ['def'],
-	description: 'Gives definition of said word!',
+	description: 'Gives definition of said word! If there are no words said, then it gives a random definition',
 	execute(message, args) {
-      var question = args.slice(0).join(" ");
-    
-      if(question === ''){
-          message.channel.send("Give me a word retard!");
-          return;
-      }
+		
+	      var question = args.slice(0).join(" ");
 
-      if(question === '.'){
-          urban.random().first(function(json) {
-            message.channel.send(`**${json.word}**\n`+ json.definition);
-            return;
-          });
-
-      }
-
-      urban(question).first(json =>{
-        
-        try{
-          message.channel.send(json.definition);
-        }catch(err){
-          message.channel.send("No results found");
-        }
-        
-        return;
-      });
+	      let settings = { method: "Get" };
+		
+	      //Random Definition
+	      if(question === ''){
+		 message.channel.send("Give me a word retard!");
+		 let url = `http://api.urbandictionary.com/v0/random`;
+		 fetch(url, settings)
+		 .then(res => res.json())
+		 .then((json) => {
+		    message.channel.send(`**${json.list[0].word}**\n`+json.list[0].definition);
+		 });
+		  return;
+	      }
+	      
+	      //Asked Definition
+	      let url = `http://api.urbandictionary.com/v0/define?term=${question}`;
+              fetch(url, settings)
+		 .then(res => res.json())
+		 .then((json) => {
+		    try{
+           		var msg = message.channel.send(json.list[0].definition);
+        	    }catch(err){
+            		message.channel.send('No definition found. :(');
+        	    }
+		 });
+	
 	},
 };
