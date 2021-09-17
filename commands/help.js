5382af72ca56d93f571126b1928d7296282eafc5
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-09-17 07:55:44 +0200

    Generalised help command for future commands

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,24 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+
+
 
 module.exports = {
 	name: 'help',
 	description: 'Shows all commands',
 	execute(message, args) {
-		
-     const commandEmbed = new Discord.MessageEmbed()
+		const commandFiles = fs.readdirSync(__dirname).filter(file => file.endsWith('.js'));
+        const commandEmbed = new Discord.MessageEmbed()
                     .setColor(0x333333)
                     .setAuthor("All Commands of DenisBot")
-                    .addFields(
-                        {name: 'Ping', value: 'If u write this, the bot answers you pong lol'},
-                        {name: 'Topic', value: 'Bot gives u a topic to talk about. 1300+ topics'},
-                        {name: 'Avatar', value: 'Shows your avatar or others'},
-                        {name: 'Hug', value: 'Send hug gifs xD'},
-                        {name: 'Roast', value: 'Roast the shit out of u or others. 200+ roasts'},
-                        {name: 'Img', value: 'Shows you an image from the internet based on your input'},
-                        {name: '8ball', value: 'Replies to your stupid ass questions'},
-                        {name: 'Define', value: 'Defines your words lol. Use . for random definition'},
-                        {name: 'Meme', value: 'Send memes from Reddit'},
-                        {name: 'Movie', value: 'Search for movies from Imdb and watch them'},
-                    )
                     .setImage('https://media1.giphy.com/media/Te4NwB59ZFn68/200.gif')
+    
+        for (const file of commandFiles) {
+            const command = require(`${__dirname}/${file}`);
+            commandEmbed.addField(command.name.substring(0,1).toUpperCase()+command.name.substring(1), command.description + '\n' + (command.aliases || ''))
+        }
                     
-                    
-                message.channel.send(commandEmbed);
-
+        message.channel.send(commandEmbed);
 
 	},
 };
