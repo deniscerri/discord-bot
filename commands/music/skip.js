aa8dd78b351ded2d1f10efaa8d95b2e3c4f1692c
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-09-18 10:42:54 +0200

    Added costum skipping functionality
    User now can skip on any song in the queue without going through them one by one

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -11,6 +11,26 @@ module.exports = {
         const server_queue = queue.get(message.guild.id);
         
         if(!server_queue) return message.channel.send('There are no songs to skip in the queue!');
+
+        var cmd = '';
+        if(args[0] != undefined){
+            cmd = args[0].toLowerCase();
+        }
+        if(Number.isInteger(parseInt(cmd))){
+            cmd = parseInt(cmd);
+            let limit = server_queue.songs.length;
+            if(cmd >= limit){
+                if(limit == 1){
+                    return message.channel.send('There are no songs to skip in the queue!');
+                }else{
+                    return message.channel.send('There are only '+(limit-1)+' songs in the queue!');
+                }
+            }else{
+                for(var i = 0; i < cmd-1; i++){
+                    server_queue.songs.shift();
+                }
+            }
+        }
         server_queue.connection.dispatcher.end();
 
     }
