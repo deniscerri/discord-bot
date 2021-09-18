213013c5693a28396e32a313cbd108ed657dcf8c
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-09-18 15:42:41 +0200

    Added more info in output

--- commands/music/now_playing.js
+++ commands/music/now_playing.js
@@ -1,4 +1,5 @@
 const index = require('../../index.js');
+const Discord = require("discord.js");
 
 module.exports = {
 	name: 'np',
@@ -8,7 +9,32 @@ module.exports = {
         const server_queue = queue.get(message.guild.id);
         
         if(server_queue){
-            message.channel.send(`🎶 Now playing **${server_queue.songs[0].title}**`);
+            let song = server_queue.songs[0];
+            let embed = new Discord.MessageEmbed()
+                .setTitle('Now Playing :musical_note:')
+            let url = new URL(song.url);
+            let videoID = new URLSearchParams(url.search);
+            embed.setThumbnail(`https://img.youtube.com/vi/${videoID.get('v')}/mqdefault.jpg`)
+
+            let description = '';
+            description += `[${song.title}](${song.url})\n\n`;
+
+            let length = 0;
+            if(server_queue.songs[0].length_seconds < 3600){
+                length = new Date(parseInt(server_queue.songs[0].length_seconds) * 1000).toISOString().substr(14, 5)
+            }else{
+                length = new Date(parseInt(server_queue.songs[0].length_seconds) * 1000).toISOString().substr(11, 8)
+            }
+            description += '`Length:` ' + length + '\n\n'; 
+            description += '`Requested by:` ' + song.requestedBy +'\n\n';
+
+            let upNext = 'Nothing';
+            if(server_queue.songs[1]){
+                upNext = server_queue.songs[1].title;
+            }
+            description += '`Up Next:` '+upNext;
+            embed.setDescription(description);
+            message.channel.send(embed);
         }else{
             message.channel.send('The queue is empty!');
         }
