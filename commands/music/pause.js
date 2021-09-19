3a045a184c30ef679d01f4de8350d36c13a55d62
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-09-19 09:04:27 +0200

    Revert "Removed ability to control the bot when being on different audio channels"
    This reverts commit a3602aeb99956af4946c79e79940d4695090f60c.

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -9,19 +9,12 @@ module.exports = {
 
         if(!voice_ch){ return message.channel.send('You need to be in a audio channel to execute this command!');}
         const server_queue = queue.get(message.guild.id);
-
-        if(!message.guild.me.voice.channel) return message.channel.send('I am not in a voice channel!');
         
-        if(message.guild.me.voice.channel == voice_ch){
-            if(!server_queue) {
-                message.channel.send('No song is playing!'); return;
-            }else{
-                server_queue.connection.dispatcher.pause(true);
-                message.channel.send(`⏸ Paused **${server_queue.songs[0].title}**`)
-            }
+        if(!server_queue) {
+            message.channel.send('No song is playing!'); return;
         }else{
-            message.channel.send('You need to be in the same audio channel as the bot to pause a song!');
+            server_queue.connection.dispatcher.pause(true);
+            message.channel.send(`⏸ Paused **${server_queue.songs[0].title}**`)
         }
-        
     }
 }
--- commands/music/skip.js
+++ commands/music/skip.js
@@ -9,41 +9,30 @@ module.exports = {
 
         if(!voice_ch){ return message.channel.send('You need to be in a audio channel to execute this command!');}
         const server_queue = queue.get(message.guild.id);
-        if(!message.guild.me.voice.channel) return message.channel.send('I am not in a voice channel!');
         
-        if(message.guild.me.voice.channel == voice_ch){
-            skip(message, server_queue, args);
-        }else{
-            message.channel.send('You need to be in the same audio channel as the bot to skip a song!');
+        if(!server_queue) return message.channel.send('There are no songs to skip in the queue!');
+
+        var cmd = '';
+        if(args[0] != undefined){
+            cmd = args[0].toLowerCase();
         }
-
-    }
-}
-
-
-function skip(message, server_queue, args){
-    if(!server_queue) return message.channel.send('There are no songs to skip in the queue!');
-
-    var cmd = '';
-    if(args[0] != undefined){
-        cmd = args[0].toLowerCase();
-    }
-    if(Number.isInteger(parseInt(cmd))){
-        cmd = parseInt(cmd);
-        let limit = server_queue.songs.length;
-        if(cmd >= limit){
-            if(limit == 1){
-                return message.channel.send('There are no songs to skip in the queue!');
+        if(Number.isInteger(parseInt(cmd))){
+            cmd = parseInt(cmd);
+            let limit = server_queue.songs.length;
+            if(cmd >= limit){
+                if(limit == 1){
+                    return message.channel.send('There are no songs to skip in the queue!');
+                }else{
+                    return message.channel.send('There are only '+(limit-1)+' songs in the queue!');
+                }
             }else{
-                let plural_song = (server_queue.songs.length == 2) ? 'song' : 'songs';
-                return message.channel.send('There are only '+(limit-1)+' '+plural_song+' in the queue!');
-            }
-        }else{
-            for(var i = 0; i < cmd-1; i++){
-                server_queue.songs.shift();
+                for(var i = 0; i < cmd-1; i++){
+                    server_queue.songs.shift();
+                }
             }
         }
+        message.channel.send('⏭ Skipping!')
+        server_queue.connection.dispatcher.end();
+
     }
-    message.channel.send('⏭ Skipping!')
-    server_queue.connection.dispatcher.end();
 }
