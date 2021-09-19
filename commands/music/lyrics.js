a3602aeb99956af4946c79e79940d4695090f60c
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-09-19 08:58:19 +0200

    Removed ability to control the bot when being on different audio channels

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -12,6 +12,12 @@ module.exports = {
         if(!voice_ch){ return message.channel.send('You need to be in a audio channel to execute this command!');}
         const server_queue = queue.get(message.guild.id);
 
+        if(!message.guild.me.voice.channel) return message.channel.send('I am not in a voice channel!');
+        
+        if(!(message.guild.me.voice.channel == voice_ch)){
+            return message.channel.send('You need to be in the same audio channel as the bot to show lyrics!');
+        }
+
         if(!server_queue){ message.channel.send('There are no songs playing!'); return;}
      
         const author = '';
