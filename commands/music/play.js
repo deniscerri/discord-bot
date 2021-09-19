3530b1f0bd3f883c1526c123b51e76b77ea96768
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-09-19 09:09:30 +0200

    Added playlist support
    Now you can add the whole playlist in the queue. If the playlist is longer than 100 elements, it will not include the ones after 100. Look through ytpl documentation to accept every element.
    The whole search portion got made into a function so that it will be used by other commands, playskip & playtop.

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,5 +1,6 @@
 const ytdl = require('ytdl-core');
 const ytsc = require('yt-search');
+const ytpl = require('ytpl');
 const Discord = require("discord.js");
 const index = require('../../index.js');
 const now_playing = require(`${__dirname}/now_playing.js`);
@@ -14,6 +15,11 @@ module.exports = {
         const queue = index.queue;
         
         if(!voice_ch){ return message.channel.send('You need to be in a audio channel to execute this command!');}
+        
+        if(!(message.guild.me.voice.channel == voice_ch) && message.guild.me.voice.channel){
+            return message.channel.send('You need to be in the same audio channel as the bot to play a song!');
+        }
+        
         const server_queue = queue.get(message.guild.id);
         if(!args.length){
             if(server_queue){
@@ -25,47 +31,14 @@ module.exports = {
             }else{
                 return message.channel.send('You need to write a song name or link first');
             }
-        } 
-        let song = {};
-        //if its a single youtube url
-        if(ytdl.validateURL(args[0])){
-            message.channel.send('🧐 Searching for: `'+args[0]+'`...');
-            const song_info = await ytdl.getInfo(args[0]);
-            song = {
-                title: song_info.videoDetails.title,
-                url: song_info.videoDetails.video_url, 
-                length_seconds: song_info.length_seconds,
-                requestedBy: message.author.username+'#'+message.author.discriminator
-            }
-        
-        
-        // if its a search query
-        }else{
-            const finder = async (query) => {
-                message.channel.send('🧐 Searching for: `'+args.join(' ')+'`...');
-                const videoResult = await ytsc(query);
-                return (videoResult.videos.length > 1) ? videoResult.videos[0] : null
-            }
-
-            const video = await finder(args.join(' '));
-            if(video){
-                song = {
-                    title: video.title, 
-                    url: video.url, 
-                    length_seconds: video.seconds, 
-                    requestedBy: message.author.username+'#'+message.author.discriminator
-                };
-            }else{
-                message.channel.send("Error finding video. ");
-                return;
-            }
-            
         }
-        add_to_queue(message, queue, server_queue, song, voice_ch);
+        let songs = await search(message, queue, server_queue, voice_ch, args);
+        if(!songs){return;}
+        add_to_queue(message, queue, server_queue, songs, voice_ch);
     },
 }
 
-async function add_to_queue(message, queue, server_queue, song, voice_ch){
+async function add_to_queue(message, queue, server_queue, songs, voice_ch){
     if(!server_queue){
         const queue_constructor = {
             voice_channel: voice_ch,
@@ -76,7 +49,7 @@ async function add_to_queue(message, queue, server_queue, song, voice_ch){
         }
 
         queue.set(message.guild.id, queue_constructor);
-        queue_constructor.songs.push(song);
+        songs.forEach(song => queue_constructor.songs.push(song))
 
         try{
             const connection  = await voice_ch.join();
@@ -87,12 +60,16 @@ async function add_to_queue(message, queue, server_queue, song, voice_ch){
             message.channel.send('There was an error connecting! ');
         }
     }else{
-        server_queue.songs.push(song);
-        return now_playing.added_to_queue(message, server_queue);
+        songs.forEach(song => server_queue.songs.push(song))
+        if(songs.length > 1){
+            return now_playing.added_to_queue(message, server_queue, 'playlist', server_queue.songs.length-1)
+        }
+        return now_playing.added_to_queue(message, server_queue, 'song', server_queue.songs.length-1);
     }
 }
 
 async function video_player(message, queue, guild, song){
+
     if(queue == undefined) {return;}
     const song_queue = queue.get(guild.id);
 
@@ -119,6 +96,72 @@ async function video_player(message, queue, guild, song){
     await now_playing.execute(message , []);
 }
 
+async function search(message, queue, server_queue, voice_ch, args){
+    let songs = []; 
+        let song = {};
+        //if its a playlist link
+        try{
+            let checkPlaylist = await ytpl.getPlaylistID(args[0]);
+            message.channel.send('🧐 Searching for playlist: `'+args[0]+'`...');
+            const results = await ytpl(checkPlaylist, {pages: 1});
+            for(var i = 0; i < results.items.length; i++){
+                try{
+                    if(results.items[i].isPlayable){
+                        let song = {
+                            title: results.items[i].title,
+                            url: results.items[i].shortUrl,
+                            length_seconds: results.items[i].durationSec,
+                            requestedBy: message.author.username+'#'+message.author.discriminator,
+                            playlist_url: results.url,
+                            playlist_title: results.title,
+                        }
+                        songs.push(song);
+                    }
+                }catch(err){
+                    continue;
+                }
+            }
+            
+        }catch(err){
+            //if its a single youtube url
+            if(ytdl.validateURL(args[0])){
+                message.channel.send('🧐 Searching for: `'+args[0]+'`...');
+                const song_info = await ytdl.getInfo(args[0]);
+                song = {
+                    title: song_info.videoDetails.title,
+                    url: song_info.videoDetails.video_url, 
+                    length_seconds: song_info.length_seconds,
+                    requestedBy: message.author.username+'#'+message.author.discriminator
+                }
+                songs.push(song);
+            // if its a search query
+            }else{
+                const finder = async (query) => {
+                    message.channel.send('🧐 Searching for: `'+args.join(' ')+'`...');
+                    const videoResult = await ytsc(query);
+                    return (videoResult.videos.length > 1) ? videoResult.videos[0] : null
+                }
+
+                const video = await finder(args.join(' '));
+                if(video){
+                    song = {
+                        title: video.title, 
+                        url: video.url, 
+                        length_seconds: video.seconds, 
+                        requestedBy: message.author.username+'#'+message.author.discriminator
+                    };
+                    songs.push(song);
+                }else{
+                    message.channel.send("Error finding video. ");
+                    return;
+                }
+                
+            }
+        }
+    return songs;
+}
+
 
 module.exports.video_player = video_player;
 module.exports.add_to_queue = add_to_queue;
+module.exports.search = search;
