26aa3d4f568ece440b87037027f061f92e9cbeaa
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-10-05 16:58:59 +0200

    Added costum google search engine for the image command
    The scraper abeit pretty useful, tends to be slower than a REST API Call. Bot will use the API if it hasn't reached its daily limit quota. If the limit has been reached, it will use the scraper as before.

--- commands/image.js
+++ commands/image.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 var Scraper = require('images-scraper');
 var Discord = require('discord.js');
+const fetch = require('node-fetch');
 
 const minimal_args = [
   '--autoplay-policy=user-gesture-required',
@@ -45,8 +46,8 @@ const google = new Scraper({
   userAgent: 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0', // the user agent
   puppeteer: {
 	headless: true,
-	args: minimal_args
-	//userDataDir : './cache'
+	args: minimal_args,
+	userDataDir : './cache'
   }
 });
 
@@ -64,41 +65,101 @@ module.exports = {
       return;
     }
 
-    var i = 0;
+    let url = `https://www.googleapis.com/customsearch/v1?key=${process.env['google_search']}&safe=medium&searchType=image&q=${search}&start=1`
     message.channel.send('🔎📷 Searching for: `'+search+'`');
-    const results = await google.scrape(search,nr);
-    var msg = message.channel.send(results[i].url)
-      .then(async function(msg){
-                msg.react('⏭')
-                msg.react('🔀');
-                await msg.awaitReactions(reaction => {
-                  if(reaction.emoji.reaction.count > 1){
-                    switch(reaction.emoji.name){
-                      case '⏭':
-                        i = ++i % results.length;
-                        break;
-                      case '⏮':
-                        i = --i % results.length;
-                        break;
-                      case '🔀':
-                        i = Math.floor(Math.random() * results.length);
-                        break;
-                    }
-                    msg.edit(results[i].url);
-                    msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
-                    if(i == results.length){
-                      msg.react('⏮')
-                    }else if(i == 0){
-                      msg.react('⏭')
-                    }else{
-                      msg.react('⏮')
-                      msg.react('⏭')
-                    }
-                    msg.react('🔀')
-                    
-                  }
-                  
-            }, {time: 600000});
-        })
+    let json = await fetch_json(url);
+    if(json.hasOwnProperty('items')){
+      costum_google_search(message, json, search);
+    }else{
+      fallback_scraper(message, search);
+    }
 	},
 };
+
+const fetch_json = async (url) => {
+  let settings = { method: "Get" };
+  let res;
+  var response = await fetch(url, settings)
+  let json = await response.json();
+  return json;
+}
+
+
+const costum_google_search = (message, json, search) => {
+  var i = 0;
+  var msg = message.channel.send(json.items[i].link)
+    .then(async function(msg){
+              msg.react('⏭')
+              msg.react('🔀');
+              await msg.awaitReactions(async reaction => {
+                if(reaction.emoji.reaction.count > 1){
+                  switch(reaction.emoji.name){
+                    case '⏭':
+                      i++;
+                      if((i % json.items.length) == 0){
+                        let url = `https://www.googleapis.com/customsearch/v1?key=${process.env['google_search']}&safe=medium&searchType=image&q=${search}&start=${(json.queries.nextPage[0].startIndex)}`
+                        json = await fetch_json(url);
+                      }
+                      break;
+                    case '⏮':
+                      i--;
+                      if(i < json.queries.request[0].startIndex - 1){
+                        let url = `https://www.googleapis.com/customsearch/v1?key=${process.env['google_search']}&safe=medium&searchType=image&q=${search}&start=${(json.queries.previousPage[0].startIndex)}`
+                        json = await fetch_json(url);
+                      }
+                      break;
+                    case '🔀':
+                      i = Math.floor(Math.random() * json.items.length);
+                      break;
+                  }
+                  msg.edit(json.items[i % 10].link);
+                  msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+                  if(json.queries.hasOwnProperty('previousPage') || i > 0){
+                    msg.react('⏮')
+                  }
+                  msg.react('⏭')
+                  msg.react('🔀')
+                  
+                }
+                
+          }, {time: 600000});
+      })
+}
+
+const fallback_scraper = async (message, search) => {
+  var i = 0;
+  const results = await google.scrape(search,nr);
+  var msg = message.channel.send(results[i].url)
+    .then(async function(msg){
+              msg.react('⏭')
+              msg.react('🔀');
+              await msg.awaitReactions(reaction => {
+                if(reaction.emoji.reaction.count > 1){
+                  switch(reaction.emoji.name){
+                    case '⏭':
+                      i = ++i % results.length;
+                      break;
+                    case '⏮':
+                      i = --i % results.length;
+                      break;
+                    case '🔀':
+                      i = Math.floor(Math.random() * results.length);
+                      break;
+                  }
+                  msg.edit(results[i].url);
+                  msg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+                  if(i == results.length){
+                    msg.react('⏮')
+                  }else if(i == 0){
+                    msg.react('⏭')
+                  }else{
+                    msg.react('⏮')
+                    msg.react('⏭')
+                  }
+                  msg.react('🔀')
+                  
+                }
+                
+          }, {time: 600000});
+      })
+}
